afe8524618cde89b025fb853d7fc270ed961a269
Sander Nieuwenhuizen <snieuwen@gmail.com>
2011-11-12 12:32:19 +0100

    Implemented user#find. User#new should not be used anymore

--- lib/last.fm/user.rb
+++ lib/last.fm/user.rb
@@ -1,17 +1,49 @@
 module LastFM
   class User
+    extend LastFM::ImageReader
     include RequestHelper
     include Unimplemented
     include Errors
     
-    unimplemented methods: [:artist_tracks, :banned_tracks, :events, :friends, :info, :loved_tracks, :neighbours,
+    # private_class_method :new
+    
+    attr_reader :id, :name, :age, :bootstrap, :country, :gender, :images, :playcount, :playlist_count, :realname, :registered, :subscriber, :type, :url
+    
+    unimplemented methods: [:artist_tracks, :banned_tracks, :events, :friends, :loved_tracks, :neighbours,
                             :past_events, :personal_tags, :playlists, :recent_stations, :recent_tracks,
                             :recommended_artists, :recommended_events, :shouts, :top_albums, :top_tags,
                             :top_tracks, :weekly_album_chart, :weekly_artist_chart, :weekly_chart_list,
                             :weekly_track_chart, :shout]
-                  
-    def initialize(name)
-      @name = name
+
+    class << self
+      def find(username)
+        xml = do_request(method: "user.getinfo", user: username)
+        self.from_xml(xml)
+      end
+      alias_method :info, :find
+      
+      def from_xml(xml)
+        user = xml.at("//user")
+        
+        return if user.nil?
+        
+        id = user.at("./id").content.to_i
+        name = user.at("./name").content
+        age = user.at("./age").content.to_i
+        bootstrap = user.at("./bootstrap").content.to_i
+        country = user.at("./country").content        
+        gender = user.at("./gender").content
+        images = images(user, "./image")
+        playcount = user.at("./playcount").content.to_i
+        playlist_count = user.at("./playlists").content.to_i
+        realname = user.at("./realname").content
+        registered = DateTime.parse(user.at("./registered").content)
+        subscriber = user.at("./subscriber").content == "1"
+        type = user.at("./type").content
+        url = user.at("./url").content
+
+        User.new(id, name, age, bootstrap, country, gender, images, playcount, playlist_count, realname, registered, subscriber, type, url)
+      end
     end
     
     def top_artists
@@ -33,6 +65,13 @@ module LastFM
       end
       
       @top_artists = top_artists
+    end
+    
+  private
+
+    def initialize(id, name, age, bootstrap, country, gender, images, playcount, playlist_count, realname, registered, subscriber, type, url)
+      @id, @name, @age, @bootstrap, @country, @gender, @images, @playcount, @playlist_count, @realname, @registered, @subscriber, @type, @url =
+        id, name, age, bootstrap, country, gender, images, playcount, playlist_count, realname, registered, subscriber, type, url
     end
   end
 end
--- spec/lib/user_spec.rb
+++ spec/lib/user_spec.rb
@@ -3,10 +3,48 @@ require 'spec_helper'
 describe LastFM::User do
   include StubResponse
 
+  describe "find" do
+    it "should retrieve user details" do
+      stub_user_response(method: "user.getinfo", user: "munkius")
+      munkius = LastFM::User.find("munkius")
+      
+      munkius.name.should == "Munkius"
+      munkius.realname.should == ""
+      munkius.images.small.should == "http://userserve-ak.last.fm/serve/34/2487647.jpg"
+      munkius.images.medium.should == "http://userserve-ak.last.fm/serve/64/2487647.jpg"
+      munkius.images.large.should == "http://userserve-ak.last.fm/serve/126/2487647.jpg"
+      munkius.images.extralarge.should == "http://userserve-ak.last.fm/serve/252/2487647.jpg"
+      munkius.url.should == "http://www.last.fm/user/Munkius"
+      munkius.id.should == 4724243
+      munkius.country.should == "NL"
+      munkius.age.should == 32
+      munkius.gender.should == "m"
+      munkius.subscriber.should be_false
+      munkius.playcount.should == 47893
+      munkius.playlist_count.should == 0
+      munkius.bootstrap.should == 0
+      munkius.registered.should == DateTime.new(2006, 9, 23, 7, 21)
+      munkius.type.should == "user"
+    end
+    
+    it "should return nil when a user does not exist" do
+      stub_user_response(method: "user.getinfo", user: "non-existent-user")
+      LastFM::User.find("non-existent-user").should be_nil
+    end
+    
+    it "should alias find and info" do
+      stub_user_response(method: "user.getinfo", user: "munkius")
+      LastFM::User.find("munkius").name.should == LastFM::User.info("munkius").name
+    end
+  end
+  
   describe "top artists" do
+    
     it "should find a user's top artists" do
-      stub_user_response(method: "user.gettopartists", user: "munkius")
-      top_artists = LastFM::User.new("munkius").top_artists
+      stub_user_response(method: "user.getinfo", user: "munkius")
+      stub_user_response(method: "user.gettopartists", user: "Munkius")
+
+      top_artists = LastFM::User.find("munkius").top_artists
       top_artists.size.should == 50
       intwine = top_artists.first
     
@@ -19,13 +57,6 @@ describe LastFM::User do
       intwine.images[:large].should == "http://userserve-ak.last.fm/serve/126/3350388.jpg"
       intwine.images[:extralarge].should == "http://userserve-ak.last.fm/serve/252/3350388.jpg"
       intwine.images[:mega].should == "http://userserve-ak.last.fm/serve/_/3350388/Intwine+nieuw1.jpg"
-    end
-  
-    it "should return empty array for non-existing user" do
-      stub_user_response(method: "user.gettopartists", user: "non-existing-user")
-      user = LastFM::User.new("non-existing-user")
-      user.top_artists.should == []
-      # user.error.should == LastFM::Errors::ERRORS[:error_6]
     end
   end
 end
